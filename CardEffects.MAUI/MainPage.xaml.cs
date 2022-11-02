b8f8e4fd57f657add97836a2b805751da9f230e7
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-11-02 15:47:08 -0500

    You can tap on a card to see it large
    TODO on some rotation

--- CardEffects.MAUI/MainPage.xaml.cs
+++ CardEffects.MAUI/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using SkiaSharp.Views.Maui;
 using SkiaSharp;
+using static System.Diagnostics.Debug;
 
 namespace CardEffects.MAUI;
 
@@ -7,8 +8,14 @@ public partial class MainPage : ContentPage
 {
 	const int Count = 20;
 	List<SKBitmap> _bitmaps = new(Count);
-	HttpClient _client = new();
-	SKPoint? _touch;
+	readonly HttpClient _client = new();
+	readonly SKPaint _paint = new SKPaint
+	{
+		IsAntialias = true,
+	};
+	SKBitmap _selected;
+	float _width, _height;
+	SKMatrix44 _matrix;
 
 	public MainPage()
 	{
@@ -41,12 +48,33 @@ public partial class MainPage : ContentPage
 
 	void OnTouch(object sender, SKTouchEventArgs e)
 	{
-		if (e.InContact)
-			_touch = e.Location;
-		else
-			_touch = null;
+		if (e.ActionType == SKTouchAction.Released)
+		{
+			if (_selected == null)
+			{
+				int index = (int)(e.Location.X / _width * 4) + (int)(e.Location.Y / _height * 5) * 4;
+				_selected = _bitmaps[index];
+			}
+			else
+			{
+				_selected = null;
+			}
+			_skia.InvalidateSurface();
+			_matrix = SKMatrix44.CreateIdentity();
+		}
+		else if (e.InContact)
+		{
+			//TODO: no idea on this yet
 
-		_skia.InvalidateSurface();
+			//var diff = new SKPoint((e.Location.X * 2 - _width) / _width, (e.Location.Y * 2 - _height) / _height);
+			//WriteLine(diff);
+			//_matrix = SKMatrix44.CreateIdentity();
+			//_matrix.PostConcat(SKMatrix44.CreateRotationDegrees(1, 0, 0, diff.X * 10));    // x
+			//_matrix.PostConcat(SKMatrix44.CreateRotationDegrees(0, 1, 0, diff.Y * 10));    // y
+			//_matrix.PostConcat(SKMatrix44.CreateRotationDegrees(0, 0, 1, 0));              // z
+
+			_skia.InvalidateSurface();
+		}
 
 		e.Handled = true;
 	}
@@ -58,19 +86,40 @@ public partial class MainPage : ContentPage
 	
 		lock (_bitmaps)
 		{
-			var src = new SKRect();
-			var dest = new SKRect(0, 0, e.Info.Width / 4, e.Info.Height / 5);
-			var location = new SKPoint();
-			for (int i = 0; i < _bitmaps.Count; i++)
+			if (_selected != null)
 			{
-				var bitmap = _bitmaps[i];
-				src.Right = bitmap.Width;
-				src.Bottom = bitmap.Height;
-				location.X = (i % 4) * dest.Size.Width;
-				location.Y = (i / 4) * dest.Size.Height;
-				dest.Location = location;
-				canvas.DrawBitmap(bitmap, src, dest);
+				DrawOne(e, canvas);
 			}
+			else
+			{
+				DrawAll(e, canvas);
+			}
+		}
+	}
+
+	void DrawOne(SKPaintSurfaceEventArgs e, SKCanvas canvas)
+	{
+		canvas.SetMatrix(_matrix.Matrix);
+		var dest = new SKRect(0, 0, e.Info.Width, e.Info.Height);
+		canvas.DrawBitmap(_selected, dest, _paint);
+	}
+
+	void DrawAll(SKPaintSurfaceEventArgs e, SKCanvas canvas)
+	{
+		_width = e.Info.Width;
+		_height = e.Info.Height;
+		var src = new SKRect();
+		var dest = new SKRect(0, 0, _width / 4, _height / 5);
+		var location = new SKPoint();
+		for (int i = 0; i < _bitmaps.Count; i++)
+		{
+			var bitmap = _bitmaps[i];
+			src.Right = bitmap.Width;
+			src.Bottom = bitmap.Height;
+			location.X = (i % 4) * dest.Size.Width;
+			location.Y = (i / 4) * dest.Size.Height;
+			dest.Location = location;
+			canvas.DrawBitmap(bitmap, src, dest, _paint);
 		}
 	}
 }
