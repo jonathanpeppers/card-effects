a1afdf2826bfbfe0fab47b743ed9bcd9ac3d07ad
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-11-02 15:46:35 -0500

    Setup Window for Windows

--- CardEffects.MAUI/App.xaml.cs
+++ CardEffects.MAUI/App.xaml.cs
@@ -8,4 +8,14 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		var window = base.CreateWindow(activationState);
+		window.Width = 754;
+		window.Height = 1044;
+		window.X = (1920 - window.Width) / 2;
+		window.Y = (1080 - window.Height) / 2;
+		return window;
+	}
 }
