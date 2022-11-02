2d6492e0f023b4f7f1579fae367368d31a902872
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-11-02 12:01:25 -0500

    Simple CollectionView with images

--- CardEffects.MAUI/MainPage.xaml.cs
+++ CardEffects.MAUI/MainPage.xaml.cs
@@ -2,23 +2,17 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		const int count = 30;
+		var list = new List<string>(count);
+		for (int i = 1; i <= count; i++)
+		{
+			list.Add ($"https://images.pokemontcg.io/sm35/{i}_hires.png");
+		}
+		_collectionView.ItemsSource = list;
 	}
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # card-effects
- TCG card effects for .NET MAUI, inspired by https://github.com/simeydotme/pokemon-cards-css
+
+This is WIP, not finished yet.
+
+TCG card effects for .NET MAUI, inspired by:
+
+https://github.com/simeydotme/pokemon-cards-css
+
+Images from:
+
+* https://pokemontcg.io/
+* https://docs.pokemontcg.io/
